8432fd456599a46bca8ae19597947d582b95fa40
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-08-25 00:08:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,12 +85,12 @@ function addEventListenerToAllButtons()
                     
                     let symbol = "O";
                     if(chanceX){
-                        // updateMark(index,'X');
+                        updateMark(index,'X');
                         symbol = "X"
                     }
                     else
                     {
-                        // updateMark(index,'O');
+                        updateMark(index,'O');
                     }
                     // send the changes into server using socket connection
                     
